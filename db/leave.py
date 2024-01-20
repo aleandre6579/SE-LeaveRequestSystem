d8932f3310a9d9b4f83de8b475adae8b359e53d0
aleandre6579 <aleandre6579.simon@gmail.com>
2024-01-20 22:16:28 +0700

    same day conflict fixed: only for your leaves

--- db/leave.py
+++ db/leave.py
@@ -1,8 +1,14 @@
 from datetime import datetime
 from SE_LeaveRequestSystem.extensions import db
 from SE_LeaveRequestSystem.db.models import LeaveRequest
+from flask import session
 
 
 def hasSameDayConflict(date: datetime.day) -> bool:
+    same_day_leaves = LeaveRequest.query.filter_by(date_start=date, id=session.get('user_id')).all()
+    return len(same_day_leaves) > 0
+
+
+def validatesDayQuota(date: datetime.day) -> bool:
     same_day_leaves = LeaveRequest.query.filter_by(date_start=date).all()
     return len(same_day_leaves) > 0
